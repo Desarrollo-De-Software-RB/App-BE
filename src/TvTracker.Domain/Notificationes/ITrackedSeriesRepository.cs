1b7e20d77f728ee27f93959582e165f506e5097c
Felipe <felipepalazzi500@gmail.com>
2026-01-24 18:56:54 -0300

    fix: Se elimino el TrackedSeries debido a que se va a hacer las notificaciones en base a la lista de seguimiento.

--- src/TvTracker.Domain/Notificationes/ITrackedSeriesRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Volo.Abp.Domain.Repositories;
-
-namespace TvTracker.Notificationes
-{
-    public interface ITrackedSeriesRepository : IRepository<TrackedSeries, Guid>
-    {
-    }
-}
